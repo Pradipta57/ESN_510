81064b216c010ed9a679c38c8e71475c1b8fbe23
Pradipta57 <140300370+Pradipta57@users.noreply.github.com>
2023-08-06 12:38:19 +0530

    Add files via upload

---
+++ Assignment_1.m
@@ -0,0 +1,16 @@
+a=34;
+b=56;
+c=78;
+d=69;
+e=input("Enter the value e\n");
+f=input("Enter the value f\n");
+g=input("Enter the value g\n");
+h=input("Enter the value h\n");
+x=a+e;
+y=b-f;
+z=c*g;
+u=d/h;
+fprintf("%d + %d =%d\n",a,e,x);
+fprintf("%d - %d =%d\n",b,f,y);
+fprintf("%d * %d =%d\n",c,g,z);
+fprintf("%d / %d =%d\n",d,h,u);
